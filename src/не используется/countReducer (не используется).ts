1da48bb6de67b6b07bbad784b9ea07eca030d648
Ni3X_BL <v.gorbach@brightlab.me>
2023-02-10 00:39:44 +0300

    preparing for deploy change package json

--- src/не используется/countReducer (не используется).ts
+++ src/не используется/countReducer (не используется).ts
@@ -1,18 +1,19 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+// import { createAction, createReducer } from "@reduxjs/toolkit";
 
-const initialState = {
-  count: 0,
-  todos: ["Снять видео", "Смонтировать видео", "Покакать"],
-};
+// const initialState = {
+//   count: 0,
+//   todos: ["Снять видео", "Смонтировать видео", "Покакать"],
+// };
 
-export const increment = createAction("INCREMENT");
-export const decrement = createAction("DECREMENT");
+// export const increment = createAction("INCREMENT");
+// export const decrement = createAction("DECREMENT");
 
-export const countReducer = createReducer(initialState, {
-  [increment]: (state) => {
-    state.count = state.count + 1;
-  },
-  [decrement]: (state) => {
-    state.count = state.count - 1;
-  },
-});
+// export const countReducer = createReducer(initialState, {
+//   [increment]: (state) => {
+//     state.count = state.count + 1;
+//   },
+//   [decrement]: (state) => {
+//     state.count = state.count - 1;
+//   },
+// });
+export const number = 5;
